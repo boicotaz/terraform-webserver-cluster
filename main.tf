d83231e09eab902d3778616939b030bb720234b4
boicotaz <tolisgerodimos94@gmail.com>
2022-03-24 18:14:03 +0200

    Create webserver Add Security group

--- main.tf
+++ main.tf
@@ -4,11 +4,29 @@ provider "aws" {
 
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "webserver" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.webserver.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Webserver is up and running" >> index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
-  Name = "terraform-example"
+    Name = "terraform-webserver"
+  }
+}
+
+resource "aws_security_group" "webserver" {
+  name = "terraform-webserver-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
