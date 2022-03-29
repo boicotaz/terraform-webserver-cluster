953a235831416c5c6f5c78a0ee7ccffce583cb8b
boicotaz <tolisgerodimos94@gmail.com>
2022-03-29 18:11:24 +0300

    Add Application Load Balancer for webserver cluster

--- main.tf
+++ main.tf
@@ -54,9 +54,12 @@ resource "aws_autoscaling_group" "webservers" {
   min_size             = 2
   launch_configuration = aws_launch_configuration.webserver.name
 
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
   tag {
     key                 = "Name"
-    value               = "webserver"
+    value               = "webserver-cluster"
     propagate_at_launch = true
   }
 }
@@ -69,5 +72,81 @@ resource "aws_security_group" "webserver" {
     to_port     = var.webserver_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "webserver-alb-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "cluster_alb" {
+  name               = "webserver-cluster-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.cluster_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page not found :("
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name        = "webserver-cluster-lb-tg"
+  target_type = "instance"
+  port        = var.webserver_port
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg-http" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "webserver_ip_address" {
-  value       = aws_instance.webserver.public_ip
-  description = "The public IP address of the web server instance."
+output "webserver_alb_address" {
+  value       = aws_lb.cluster_alb.dns_name
+  description = "The public DNS of the web server cluster load balancer."
+}
+
+#output "webserver_cluster_ip_addresses" {
+#  value       = aws_autoscaling_group.webservers[*].public_ip
+#  description = "The public IP addresses of the web server cluster instances."
+#}
+
+output "debug" {
+  value       = data.aws_subnets.default.ids
+  description = "Debug"
 }
