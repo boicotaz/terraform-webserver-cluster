89ed57c7a292fd51e24d4b4ef4049a06251fbd0a
boicotaz <tolisgerodimos94@gmail.com>
2022-03-31 17:17:37 +0300

    Add asg name and alb sg id as module outputs

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -2,3 +2,13 @@ output "alb_dns" {
   value       = aws_lb.cluster_alb.dns_name
   description = "The public DNS of the web server cluster load balancer."
 }
+
+output "asg_name" {
+  value       = aws_autoscaling_group.webservers.name
+  description = "The name of the Auto Scaling Group"
+}
+
+output "alb_security_group_id" {
+  value       = aws_security_group.alb.id
+  description = "The ID of the Security Group attached to the load balancer"
+}
